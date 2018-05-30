87c61ec816ee7f5cf0e12b2f7a3aee96f24d3a40
David <davidmoreiraalmeida@hotmail.com>
2018-05-30 19:48:33 +0100

    AXI peripheral started

---
+++ ip_repo/ComputeNewCentroids_1.0/src/PickCentroid.vhd
@@ -0,0 +1,50 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.projectPackage.ALL;
+
+entity PickCentroid is
+    generic(
+        NUM_FEATURES: integer := 4;
+        NUM_CENTROIDS: integer := 2
+    );
+    port(
+        clk: in std_logic;
+        reset: in std_logic;
+        features_in: in point_array(NUM_FEATURES-1 downto 0);
+        centroids: in point_array(NUM_CENTROIDS*NUM_FEATURES-1 downto 0);
+        centroid: out integer := -1
+    );
+end PickCentroid;
+
+architecture Behavioral of PickCentroid is
+
+signal s_distances: int_array(NUM_CENTROIDS-1 downto 0);
+
+begin
+    EuclideanDistance_GEN:  for i in 0 to NUM_CENTROIDS-1 generate
+            EuclideanDistanceX: entity work.EuclideanDistance
+                generic map
+                    (
+                        NUM_FEATURES => NUM_FEATURES
+                    )
+                port map(
+                    clk => clk,
+                    features => features_in,
+                    centroid_features => centroids((i+1)*NUM_FEATURES-1 downto i*NUM_FEATURES),
+                    distance => s_distances(i)
+                );
+    end generate EuclideanDistance_GEN;
+    
+    PickSmallest: entity work.PickSmallest
+        generic map
+        (
+            NUM_CENTROIDS => NUM_CENTROIDS
+        )
+        port map(
+            clk => clk,
+            reset => reset,
+            distances => s_distances,
+            centroid => centroid
+        );
+   
+end Behavioral;
---
+++ ip_repo/ComputeNewCentroids_1.0/src/projectPackage.vhd
@@ -0,0 +1,13 @@
+library IEEE; 
+use IEEE.STD_LOGIC_1164.all; 
+use ieee.numeric_std.all;
+
+package projectPackage is
+    subtype point is signed(31 downto 0);
+    type point_array is array (integer range <>) of point;
+    type int_array is array (integer range<>) of integer;
+end projectPackage; 
+
+package body projectPackage is 
+
+end projectPackage; 
